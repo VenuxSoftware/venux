97508ac738890ec141b94a9859d1d9d5f1b252bd
Eugene Nosko <coder@venux.com>
2017-01-10 08:20:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+  Status: prototype
+  Process: API generation
+*/
+
+function __consolePrintHandle__(msg){
+	print(msg);
+}
+
+function $DONE(){
+	if(!arguments[0])
+		__consolePrintHandle__('Test262:AsyncTestComplete');
+	else
+		__consolePrintHandle__('Error: ' + arguments[0]);
+}
